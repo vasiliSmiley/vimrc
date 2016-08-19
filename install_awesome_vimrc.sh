75a824538a2495b1791d7a62caac7bb8ca1f7dd1
Vasili <vasili.smiley@gmail.com>
2016-08-19 16:48:49 +0300

    added you_complete_me

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -12,4 +12,7 @@ source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
+sudo apt-get install build-essential cmake python-dev python3-dev
+python3.5 ~/.vim_runtime/sources_non_forked/you_complete_me/install.py --gocode-completer
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
